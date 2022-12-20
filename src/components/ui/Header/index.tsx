d6a7963e4abd3076bacc33e73ddb678563defbe5
Aryan Mehta <aryanmehta2016@gmail.com>
2022-12-20 18:10:36 +0000

    added header image and is responsive

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -4,7 +4,7 @@ import { Logo } from '../Logo'
 
 function Header() {
   return (
-    <header className='bg-black h-24 flex items-center justify-between px-10 max-w-5xl mx-auto'>
+    <header className='fixed h-24 flex items-center justify-between px-10 w-full max-w-5xl left-[50%] -translate-x-[50%] border-b-[1px] border-b-grey z-10'>
       <div className="max-lg:flex max-lg:flex-1 flex-row sm:gap-16 ">
         <MenuButton />
         <div className="max-lg:flex max-lg:flex-1 max-sm:justify-center">
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Intro from './components/Intro'
+
+
+function Home() {
+
+    
+
+  return (
+    <div className='w-screen min-h-screen'>
+        <Intro />
+    </div>
+  )
+}
+
+export default Home
