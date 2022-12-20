b515cf59e5320be0b29351e52d6720712958c86a
Aryan Mehta <aryanmehta2016@gmail.com>
2022-12-20 16:17:04 +0000

    added Header

--- src/components/ui/Buttons/index.tsx
+++ src/components/ui/Buttons/index.tsx
@@ -1,4 +1,6 @@
 import Arrow from '../../../assets/shared/desktop/icon-arrow-right.svg'
+import Cart from '../../../assets/shared/desktop/icon-cart.svg'
+import './index.css'
 export const PrimaryButton = () => {
     return(
         <button className="h-12 w-40 bg-peru text-white hover:bg-desaturated-peru transition-colors">
@@ -24,6 +26,22 @@ export const TransparentButton = () => {
         <button className='h-12 w-40 flex items-center justify-center gap-2 uppercase text-sub-title hover:text-peru text-[#00000050] transition-colors'>
             <span className='font-semibold'>Shop</span>
             <img src={Arrow} alt="" />
+        </button>
+    )
+}
+
+export const MenuButton = () => {
+    return(
+        <button className='hamburger_menu'>
+            <div className="pointer-events-none line"></div>
+        </button>
+    )
+}
+
+export const CartButton = () => {
+    return(
+        <button>
+            <img src={Cart} alt="" className='pointer-events-none'/>
         </button>
     )
 }
