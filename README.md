2085a8fc16736f3c25db306beb72d4c9ee43f248
Aryan Mehta <aryanmehta2016@gmail.com>
2023-01-17 14:25:35 +0000

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Audiophile web application from frontendmentor.io
-
-This is a project by frontendmentor.io which has been developed using React.js, React Redux, TypeScript, and TailwindCSS.
--- src/pages/ErrorPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ErrorPage() {
-  return (
-    <div className='flex items-center justify-center w-full h-full'>ErrorPage</div>
-  )
-}
-
-export default ErrorPage
